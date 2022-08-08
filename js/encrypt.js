69c8cd061742970f71c710c3b701ce6f81d69737
Gaiska Salomon <stellardevgeek@gmail.com>
2022-08-08 17:16:15 -0500

    Archivos del encriptador

---
+++ js/encrypt.js
@@ -0,0 +1,43 @@
+function encrypt(){
+    var text = document.getElementById("textInput").value.toLowerCase();
+    if(text == ''){
+        document.getElementById("withoutText").style.display = "flex";
+        document.getElementById("withText").style.display = "none";
+        return;
+    }else{
+        var encryptText = text.replace(/e/igm, "enter");
+        var encryptText = encryptText.replace(/i/igm, "imes");
+        var encryptText = encryptText.replace(/a/igm, "ai");
+        var encryptText = encryptText.replace(/o/igm, "ober");
+        var encryptText = encryptText.replace(/u/igm, "ufat");
+    }
+
+    document.getElementById("withoutText").style.display = "none";
+    document.getElementById("withText").style.display = "flex";
+    document.getElementById("textEncrypted").innerHTML = encryptText;
+}  
+
+function decrypt(){
+    var text = document.getElementById("textInput").value.toLowerCase();
+    if(text == ''){
+        document.getElementById("withoutText").style.display = "flex";
+        document.getElementById("withText").style.display = "none";
+        return;
+    }else{
+        var encryptText = text.replace(/enter/igm, "e");
+        var encryptText = encryptText.replace(/imes/igm, "i");
+        var encryptText = encryptText.replace(/ai/igm, "a");
+        var encryptText = encryptText.replace(/ober/igm, "o");
+        var encryptText = encryptText.replace(/ufat/igm, "u");
+    }
+    document.getElementById("withoutText").style.display = "none";
+    document.getElementById("withText").style.display = "flex";
+    document.getElementById("textEncrypted").innerHTML = encryptText;   
+}
+
+function copy(){
+    var content = document.querySelector("#textEncrypted");
+    content.select();
+    document.execCommand("copy");
+
+}
